397851c5e3a7410cf0596fa7556693501bf57440
Mec <82681280+littleyetz@users.noreply.github.com>
2022-04-29 10:54:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,32 +2,9 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.postgres://ileoumisvssnws:2535aa6278c52520df9551ec93dc87f92b0ae3c2189cd2c192adcc920fc0eefb@ec2-34-194-73-236.compute-1.amazonaws.com:5432/d9em11g536asfs,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
