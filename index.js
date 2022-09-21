4f64e00d465b28a0dc3b848d72d7db31c4745e1d
PabloProta <antoniniprota@gmail.com>
2022-09-20 21:16:27 -0300

    #7 Criado a conexão com o Banco

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
+require('dotenv').config({
+    path: './.env'
+})
+const { query } = require('./scripts/connection.js')
 const express = require('express')
 const PORT = process.env.PORT || 8080
 const app = express()
 const http = require('http').Server(app)
 
 
-app.set('port',PORT)
+app.set('port', PORT)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
-app.get('/dia', (req, res) => {
-    res.send('hello dia!')
+app.get('/canditado/bolsonaro/tweets/total', (req, res) => {
+    query(`SELECT * FROM TCC_PROD LIMIT 100`)
 })
-
-app.get('/noite', (req, res) => {
-    res.send('hello noite!')
-  })
 
 http.listen(app.get('port'), () => {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 })
